08284a34979c27939117aeccd16b6cc1596356b2
HArbietoCortez <arbietohaydee@hotmail.com>
2015-11-13 20:18:36 +0100

    Har ändrar EcomerceTest med @RunWith(MockitoJUnitRunner.class)

--- WebshopTest/test/se/grouprich/webshop/service/ECommerceTest.java
+++ WebshopTest/test/se/grouprich/webshop/service/ECommerceTest.java
@@ -3,7 +3,10 @@ package se.grouprich.webshop.service;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ECommerceTest {
 
 	@Test
