c291c8d4957252a412c4935a612d4aa99a65a125
lindabjalla <lindabjalla@gmail.com>
2015-11-16 02:52:36 +0100

    Testat om en tom shoppingCart inte accepteras.

--- WebshopTest/test/se/grouprich/webshop/service/ECommerceTest.java
+++ WebshopTest/test/se/grouprich/webshop/service/ECommerceTest.java
@@ -12,15 +12,17 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import se.grouprich.webshop.exception.CustomerRegistrationException;
+import se.grouprich.webshop.exception.OrderException;
+import se.grouprich.webshop.exception.PaymentException;
 import se.grouprich.webshop.model.Customer;
 import se.grouprich.webshop.model.Order;
 import se.grouprich.webshop.model.Product;
+import se.grouprich.webshop.model.ShoppingCart;
 import se.grouprich.webshop.repository.Repository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ECommerceTest
 {
-
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 
@@ -40,8 +42,8 @@ public class ECommerceTest
 		exception.expect(CustomerRegistrationException.class);
 		exception.expectMessage(equalTo("You can't have a name that is longer than 30 characters"));
 
-		String email = "arbieto@gmail.com";
-		String password = "abcde";
+		String email = "aa@aa.com";
+		String password = "secret";
 		String firstName = "Haydeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 		String lastName = "Arbietoooooooooooooooooooooooooooooooo";
 
@@ -49,5 +51,18 @@ public class ECommerceTest
 
 		assertTrue(firstName.length() > 30);
 		assertTrue(lastName.length() > 30);
+	}
+
+	@Test
+	public void orderThatLacksOrderRowsShouldNotBeAccepted() throws CustomerRegistrationException, PaymentException, OrderException
+	{
+		exception.expect(OrderException.class);
+		exception.expectMessage(equalTo("Shopping cart is empty"));
+		
+		Customer customer = new Customer("aa@aa.com", "secret", "Haydee", "Arbeito");
+		ShoppingCart shoppingCart = new ShoppingCart();
+		eCommerceService.pay(customer, shoppingCart);
+		
+		assertTrue(shoppingCart.getProducts().isEmpty());
 	}
 }
