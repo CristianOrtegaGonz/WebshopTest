5c1707addbf2dc01654ca7a801b0cc3766f13eea
lindabjalla <lindabjalla@gmail.com>
2015-11-20 07:55:20 +0100

    Har snyggat till isLengthWithinRange validation

--- WebshopTest/test/se/gouprich/webshop/service/validation/CustomerValidatorTest.java
+++ WebshopTest/test/se/gouprich/webshop/service/validation/CustomerValidatorTest.java
@@ -35,15 +35,21 @@ public class CustomerValidatorTest
 		String password2 = "aa12&";
 		String password3 = "Aaa2&";
 		String password4 = "Aa122";
+		String password5 = " ";
+		String password6 = null;
 		
 		boolean valid1 = customerValidator.isValidPassword(password1);
 		boolean valid2 = customerValidator.isValidPassword(password2);
 		boolean valid3 = customerValidator.isValidPassword(password3);
 		boolean valid4 = customerValidator.isValidPassword(password4);
+		boolean valid5 = customerValidator.isValidPassword(password5);
+		boolean valid6 = customerValidator.isValidPassword(password6);
 		
 		assertTrue(valid1);
 		assertFalse(valid2);
 		assertFalse(valid3);
 		assertFalse(valid4);
+		assertFalse(valid5);
+		assertFalse(valid6);
 	}
 }
